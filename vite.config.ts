5d2a9a55fd0b93a91c4ff272efa61b6de81d170f
abloom25 <evereen2023@gmail.com>
2025-12-16 15:41:58 +0900

    添加GithubPages支持

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,13 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  // 如果部署到 GitHub Pages，需要将 base 设置为仓库名称
+  // 例如：base: '/ech0-website/',
+  base: '/',
 })
